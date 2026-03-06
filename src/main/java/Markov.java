78a87ab01b24eb10de176ff24768d87dfca866f8
Tyler Fleming <tfleming@csumb.edu>
2026-03-05 20:18:13 -0800

    Fixed getSentence()

--- src/main/java/Markov.java
+++ src/main/java/Markov.java
@@ -1,3 +1,9 @@
+/*
+Title: Markov.java
+Abstract: Program that randomly generates sentences based off words from an input file.
+Author: Tyler Fleming
+Date: 03/05/2026
+ */
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
@@ -9,31 +15,57 @@ public class Markov{
     private HashMap<String, ArrayList<String>> words;
     private String prevWord;
 
+
+    /**
+     * Constructor for Markov Class
+     *
+     * Initializes instance variables.
+     *
+     */
     public Markov(){
         words = new HashMap<>();
         words.put(BEGINS_SENTENCE, new ArrayList<>());
         prevWord = BEGINS_SENTENCE;
     }
 
+    /**
+     * Builds sentences from our word list.
+     *
+     * Gets a random word from the beginning sentence list
+     * then uses that word to generate more.
+     *
+     * @return String - the sentence randomly generated
+     */
     public String getSentence(){
         StringBuilder sentence = new StringBuilder();
         String currentWord = randomWord(BEGINS_SENTENCE);
         sentence.append(currentWord).append(" ");
 
+        int maxWords = 100;
+        int count = 0;
+
         do {
             currentWord = randomWord(currentWord);
             sentence.append(currentWord).append(" ");
-
+            count++;
            // List<String> keysAsList = new ArrayList<>(words.keySet());
            // currentWord = keysAsList.get((int)(Math.random() * keysAsList.size()));
-        }while (!endsWithPunctuation(currentWord));
+        }while (!endsWithPunctuation(currentWord) && count < maxWords);
 
         return sentence.toString().trim();
     }
 
-    public void addFromFile(String s)  {
+
+    /**
+     * Fills out word list from a file line by line.
+     *
+     * Throws IOException for wrong file name.
+     *
+     * @param file the file used
+     */
+    public void addFromFile(String file)  {
         try{
-            File f = new File(s);
+            File f = new File(file);
             Scanner in = new Scanner(f);
             while (in.hasNextLine())
                 addLine(in.nextLine());
@@ -68,14 +100,29 @@ public class Markov{
     }
 
     void addLine(String line){
+        line = line.replaceAll("\n", "").replaceAll("\t", "");
         if (line.isEmpty()) return;
-
         String[] split = line.split(" ");
 
-        for (String s : split)
+
+        for (String s : split) {
+            s = s.replaceAll(" ", "");
+            if (s.isEmpty()) continue;
             addWord(s);
+        }
     }
 
+
+    /**
+     * Checks if a word ends with punctuation.
+     *
+     * A word ends with punctuation if the last
+     * character is '.', '!', or '?'.
+     *
+     * @param word the word considered
+     * @return true if the word ends with punctuation;
+     * false otherwise
+     */
     public static boolean endsWithPunctuation(String word){
         for (int i = 0; i < PUNCTUATION_MARKS.length();i++)
             if (word.endsWith(PUNCTUATION_MARKS.charAt(i) + "")) {

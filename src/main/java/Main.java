2ddce7b4b9f7853133194584f715bf8992e7de66
Tyler Fleming <tfleming@csumb.edu>
2026-03-05 20:21:08 -0800

    spam testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args ){
         Markov markov = new Markov();
+        markov.addFromFile("hamlet.txt");
         markov.addFromFile("spam.txt");
-      /*  markov.addFromFile("cloudy.txt");
+        markov.addFromFile("cloudy.txt");
         markov.addFromFile("phrases.txt");
         markov.addFromFile("azkaban.txt");
-        markov.addFromFile("hamlet.txt");
+
         markov.addFromFile("twok.txt");
-*/
+
         System.out.println(markov);
         for (int i = 0; i < 10; i ++){
             System.out.println(markov.getSentence());
